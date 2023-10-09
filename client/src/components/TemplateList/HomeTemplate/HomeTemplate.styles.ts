b2ca32b395971988a1a28a382a9b3f7deef2971d
SuJin <kimsujin001127@gmail.com>
2023-10-09 20:42:32 +0900

    feat: 기본 컴포넌트 생성

---
+++ client/src/components/TemplateList/HomeTemplate/HomeTemplate.styles.ts
@@ -0,0 +1,23 @@
+import { COLORS } from '@/styles/colors';
+import { flex } from '@/styles/tokens';
+import { TYPO } from '@/styles/typo';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const Container = styled.div``;
+
+export const InfoBox = styled.div``;
+
+export const TitleBox = styled.div``;
+
+export const DateBox = styled.div``;
+
+export const PlaceBox = styled.div``;
+
+export const NoteBox = styled.div``;
+
+export const PeopleBox = styled.div``;
+
+export const PersonInfo = styled.div``;
+
+export const PlusBox = styled.div``;

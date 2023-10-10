5a780e774487977af798afffde6f29991723b11d
Jun99uu_Lee <igun0423@naver.com>
2023-10-10 23:33:49 +0900

    fix: storybook webpack bug fix (css-prop)

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;

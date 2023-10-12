2a0e5a5e55613528f5d29395a491b48d4c7f31f8
ChoiSangwon <fdsa200@naver.com>
2023-10-12 22:47:27 +0900

    feat : profile 구현

---
+++ client/src/components/Profile/index.tsx
@@ -0,0 +1,59 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import { TYPO } from '@/styles/typo';
+import Logo from '../../../public/assets/companions/logo-blue.svg';
+
+interface ModalProps {
+  /**
+   * 이름
+   */
+  name: string;
+  /**
+   * 학번
+   */
+  memberNo: string;
+  /**
+   * 도서관 id
+   */
+  id: string;
+}
+
+const Profile = ({ name, id, memberNo }: ModalProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const lid = id;
+  return (
+    <Container>
+      <Image width={50} src={Logo} alt="profileLogo" />
+      <InfoDiv>
+        <Name>{name}님, 반가워요!</Name>
+        <MemberNo>{memberNo}</MemberNo>
+      </InfoDiv>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 66px;
+`;
+
+const InfoDiv = styled.div`
+  margin-left: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Name = styled.div`
+  ${TYPO.title3.Sb};
+`;
+
+const MemberNo = styled.div`
+  font-family: Pretendard Variable;
+  font-size: 13px;
+  font-style: normal;
+`;
+
+export default Profile;

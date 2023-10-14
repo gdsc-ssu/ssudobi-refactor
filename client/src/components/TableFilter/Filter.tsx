c3cdafd15905e1818bfe4902dc6575506512e6a4
SuJin <kimsujin001127@gmail.com>
2023-10-14 17:15:22 +0900

    feat: props 데이터로 수정

--- client/src/components/TableFilter/Filter.tsx
+++ client/src/components/TableFilter/Filter.tsx
@@ -5,7 +5,12 @@ import SlideDown from '../../../public/images/TableFilter/SlideDown.svg';
 import SlideTop from '../../../public/images/TableFilter/SlideTop.svg';
 import React from 'react';
 
-export default function Filter() {
+interface FProps {
+  time: string;
+  place: string;
+}
+
+export default function Filter({ time, place }: FProps) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const handleButtonClick = () => {
@@ -18,11 +23,11 @@ export default function Filter() {
           <styles.FilterHeaderDiv expanded={isExpanded}>
             <styles.FilterHeaderHalf>
               <styles.FilterHeaderText>사용시간</styles.FilterHeaderText>
-              <styles.FilterText>120분</styles.FilterText>
+              <styles.FilterText>{time}분</styles.FilterText>
             </styles.FilterHeaderHalf>
             <styles.FilterHeaderHalf>
               <styles.FilterHeaderText>장소종류</styles.FilterHeaderText>
-              <styles.FilterText>세미나실</styles.FilterText>
+              <styles.FilterText>{place}</styles.FilterText>
             </styles.FilterHeaderHalf>
           </styles.FilterHeaderDiv>
 

feb73d68275207d5af929524a92caed747be67af
Jun99uu_Lee <igun0423@naver.com>
2023-10-14 21:20:47 +0900

    fix: temp cicd test

--- client/src/components/Modal/common.tsx
+++ client/src/components/Modal/common.tsx
@@ -11,7 +11,6 @@ export const ModalContent = styled.div`
   border-radius: 1rem;
   width: 30rem;
   text-align: center;
-
   box-shadow:
     rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
     rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;

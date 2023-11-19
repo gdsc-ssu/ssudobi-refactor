386f896d6bb1491142f89b68d27333b73dbecdfb
ChoiSangwon <fdsa200@naver.com>
2023-11-19 14:43:01 +0900

    feat : 예약 페이지 비즈니스 모델 이식

---
+++ client/src/components/Timetable/index.tsx
@@ -0,0 +1,345 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import Loading from '@/assets/svg/loading.svg';
+import { TYPO } from '@/styles/typo';
+
+interface TimeSlot {
+  [time: string]: boolean;
+}
+interface WeeklyData {
+  [date: string]: TimeSlot;
+}
+interface TProps {
+  processData: WeeklyData[];
+  curProcessDataIdx: number;
+  isOpenSeminar: boolean;
+  isSelected: boolean;
+  setIsSelected: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedSlots: string[];
+
+  setSelectedSlots: React.Dispatch<React.SetStateAction<string[]>>;
+  curTime: string;
+  dates: string[];
+}
+
+//[2-1] 시간표 불러오기 및 출력
+const Schedule: React.FC<TProps> = ({
+  processData,
+  isOpenSeminar,
+  isSelected,
+  setIsSelected,
+  selectedSlots,
+  setSelectedSlots,
+  curProcessDataIdx,
+  curTime,
+  dates,
+}) => {
+  const days = ['', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+
+  const [isDataAllFalse, setIsDataAllFalse] = useState(false);
+
+  const SeminarTimes = [
+    '09',
+    ' ',
+    '10',
+    ' ',
+    '11',
+    ' ',
+    '12',
+    ' ',
+    '13',
+    ' ',
+    '14',
+    ' ',
+    '15',
+    ' ',
+    '16',
+    ' ',
+    '17',
+    ' ',
+    '18',
+    ' ',
+    '19',
+    ' ',
+    '20',
+    ' ',
+  ];
+
+  const OpenSeminarTimes = [
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23',
+  ];
+
+  if (processData.length === 0) {
+    return (
+      <LoadingContainer>
+        <Loading />
+        <div style={{ height: '20px' }}></div>
+        <LoadingText>세미나룸 예약 가능 시간을</LoadingText>
+        <LoadingText>찾고있어요!</LoadingText>
+      </LoadingContainer>
+    );
+  } else {
+    return (
+      <Container>
+        <Table>
+          <Thead>
+            <TableTr>
+              {days.map((day, idx) => {
+                return <TableTh key={idx}>{day}</TableTh>;
+              })}
+            </TableTr>
+            <TableTr>
+              {dates.map((day, idx) => {
+                return <TableTh key={idx}>{day}</TableTh>;
+              })}
+            </TableTr>
+          </Thead>
+          <BodyTable>
+            {isOpenSeminar ? (
+              OpenSeminarTimes.map((time, idx) => {
+                return (
+                  <TableTr key={idx}>
+                    <TableTh>{time}&nbsp;</TableTh>
+                    <TableTd isEmpty={true}>{}</TableTd>
+                    <TableTd isEmpty={true}>{}</TableTd>
+                    <TableTd isEmpty={false}>{}</TableTd>
+                    <TableTd isEmpty={true}>{}</TableTd>
+                    <TableTd isEmpty={false}>{}</TableTd>
+                  </TableTr>
+                );
+              })
+            ) : (
+              <TableTr>
+                <TableTh>
+                  {SeminarTimes.map((time) => {
+                    return <TableTimeBox key={time}>{time}</TableTimeBox>;
+                  })}
+                </TableTh>
+                {isDataAllFalse ? (
+                  <>
+                    <TableTd
+                      key="empty-1"
+                      isEmpty={false}
+                      style={{
+                        background: 'rgba(0, 0, 0, 0.50)',
+                        border: 'none',
+                      }}
+                    ></TableTd>
+                    <TableTd
+                      key="2001"
+                      isEmpty={false}
+                      style={{
+                        background: 'rgba(0, 0, 0, 0.50)',
+                        border: 'none',
+                      }}
+                    ></TableTd>
+                    <TableTd
+                      key="2002"
+                      isEmpty={false}
+                      style={{
+                        background: 'rgba(0, 0, 0, 0.50)',
+                        border: 'none',
+                      }}
+                    ></TableTd>
+                    <TableTd
+                      key="2003"
+                      isEmpty={false}
+                      style={{
+                        background: 'rgba(0, 0, 0, 0.50)',
+                        border: 'none',
+                      }}
+                    ></TableTd>
+                    <TableTd
+                      key="2004"
+                      isEmpty={false}
+                      style={{
+                        background: 'rgba(0, 0, 0, 0.50)',
+                        border: 'none',
+                      }}
+                    ></TableTd>
+
+                    <EmptyTimeTableText>예약이 불가능해요</EmptyTimeTableText>
+                  </>
+                ) : (
+                  <>
+                    {Object.entries(processData[curProcessDataIdx]).map(
+                      ([date, timeSlots], idx) => (
+                        <TableTd key={idx} isEmpty={false}>
+                          {Object.entries(timeSlots)
+                            .sort(([timeA], [timeB]) =>
+                              timeA.localeCompare(timeB),
+                            )
+                            .map(([time, value], idx) => {
+                              const slotKey = `${date}-${time}`;
+                              const isSlotSelected =
+                                selectedSlots.includes(slotKey);
+                              const nextSlots = Object.entries(timeSlots)
+                                .sort(([timeA], [timeB]) =>
+                                  timeA.localeCompare(timeB),
+                                )
+                                .slice(
+                                  idx + 1,
+                                  idx + Math.floor(parseInt(curTime) / 30),
+                                )
+                                .map(([time, nextValue]) => ({
+                                  slot: `${date}-${time}`,
+                                  value: nextValue,
+                                }));
+
+                              const isSelectable = nextSlots.every(
+                                (slot) =>
+                                  !selectedSlots.includes(slot.slot) &&
+                                  slot.value,
+                              );
+
+                              return (
+                                <DisplayBox
+                                  key={slotKey}
+                                  value={value}
+                                  selected={isSlotSelected}
+                                  type={idx}
+                                  onClick={() => {
+                                    if (!value) return;
+
+                                    if (isSlotSelected) {
+                                      setSelectedSlots([]);
+                                    } else if (isSelectable) {
+                                      if (isSelected) return;
+                                      const newSelectedSlots = [
+                                        slotKey,
+                                        ...nextSlots.map((slot) => slot.slot),
+                                      ];
+                                      if (
+                                        newSelectedSlots.length !==
+                                        Math.floor(parseInt(curTime) / 30)
+                                      ) {
+                                        return;
+                                      }
+                                      setSelectedSlots(newSelectedSlots);
+                                      setIsSelected((res: boolean) => !res);
+                                    }
+                                  }}
+                                >
+                                  &nbsp;
+                                </DisplayBox>
+                              );
+                            })}
+                        </TableTd>
+                      ),
+                    )}
+                  </>
+                )}
+              </TableTr>
+            )}
+          </BodyTable>
+        </Table>
+        <DetailTextBox>예약할 시간을 선택해주세요</DetailTextBox>
+      </Container>
+    );
+  }
+};
+
+export default Schedule;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const DetailTextBox = styled.div`
+  color: #1d9bf0;
+  display: flex;
+  margin: auto;
+  align-items: center;
+  justify-content: center;
+  height: 5rem;
+`;
+
+const Table = styled.table`
+  border-spacing: 0;
+  max-width: 36rem;
+  width: 100%;
+`;
+
+const Thead = styled.thead``;
+
+const TableTr = styled.tr``;
+
+const TableTh = styled.th`
+  font-size: 1.1rem;
+  vertical-align: top;
+`;
+
+const DisplayBox = styled.div<{
+  value: boolean;
+  type: number;
+  selected: boolean;
+}>`
+  height: 24px;
+  border-bottom: ${(props) =>
+    props.type % 2 === 1
+      ? props.type === 23
+        ? 'none'
+        : '1px solid #b5b5b5 '
+      : '1px dotted #b5b5b5'};
+  background-color: ${(props) =>
+    props.selected ? '#6ABCF5' : props.value ? '#D7EAFC' : 'white'};
+`;
+
+const TableTd = styled.div<{
+  isEmpty: boolean;
+}>`
+  display: table-cell;
+  background-color: ${(props) => (props.isEmpty ? '#D7EAFC' : 'white')};
+  width: 6rem;
+  border: 1px solid #b5b5b5;
+`;
+
+const TableTimeBox = styled.div`
+  height: 24px;
+  margin-top: -0.25px;
+`;
+
+const BodyTable = styled.tbody``;
+
+const EmptyTimeTableText = styled.div`
+  position: absolute;
+  bottom: 50%;
+  left: 40%;
+  color: white;
+  color: #fff;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -0.8px;
+`;
+
+const LoadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-top: 150px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoadingText = styled.div`
+  ${TYPO.text1.Md};
+  margin: 0 auto;
+`;
---
+++ client/src/pages/create/timetable.tsx
@@ -0,0 +1,351 @@
+import { NavHeader } from '@/components/Layouts/Header';
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import {
+  processAvailabilityData,
+  getTimeTable,
+  WeeklyData,
+  RoomData,
+} from '@/components/Timetable/getTimeTable';
+import Schedule from '@/components/Timetable';
+import ReserveConfirmBottomModal from '@/components/BottomModal/ReserveConfirm';
+import LeftArrow from '@/assets/svg/leftArrow.svg';
+import RightArrow from '@/assets/svg/rightArrow.svg';
+
+import { RESERVE_TIME } from '@/constants/reserveTime';
+import { ROOM_USE_SECTION } from '@/constants/roomUseSection';
+import { SEMINA_AVAILABLE_PEOPLE } from '@/constants/seminaAvailablePeople';
+import { CompanionProps } from '@/utils/types/Companion';
+import { ReserveError } from '@/utils/types/ReserveError';
+import ConfirmModal from '@/components/Modal/Confrim';
+import { TYPO } from '@/styles/typo';
+import { COLORS } from '@/styles/colors';
+
+const Timetable = () => {
+  const isKeyOfReserveTime = (
+    key: string,
+  ): key is keyof typeof RESERVE_TIME => {
+    return key in RESERVE_TIME;
+  };
+
+  const route = useRouter();
+  const timeQuery = route.query.time as string;
+  const useCaseQuery = route.query.useCase as string;
+  const { companions } = route.query;
+  const [curCompanions, setCurCompanions] = useState<CompanionProps[]>([]);
+  const [personCount, setPersonCount] = useState<number>(0);
+  // URI 디코딩 후 JSON 파싱
+
+  const time = isKeyOfReserveTime(timeQuery)
+    ? RESERVE_TIME[timeQuery]
+    : undefined;
+
+  const [isOpenSeminar, setIsOpenSeminar] = useState<boolean>(false);
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const [roomData, setRoomData] = useState<RoomData>();
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [isError, setIsError] = useState<ReserveError>({
+    isError: false,
+    errorMessage: '',
+  });
+
+  const [curProcessDataIdx, setCurProcessDataIdx] = useState<number>(0);
+
+  const [processData, setProcessData] = useState<WeeklyData[]>([]);
+  const [selectedSlots, setSelectedSlots] = useState<string[]>([]);
+  const [dates, setDates] = useState<string[]>([]);
+
+  const [seminaRoom, setSeminaRoom] = useState<string[]>([]);
+
+  async function getScheduleData(curSemina: string[]) {
+    const roomdata = await getTimeTable();
+    if (roomdata === undefined) return;
+    setRoomData(roomdata as RoomData);
+    const pr = processAvailabilityData(roomdata, curSemina);
+    console.log(pr);
+    setProcessData(pr);
+  }
+
+  const getDays = () => {
+    if (
+      processData === null ||
+      processData === undefined ||
+      processData.length <= curProcessDataIdx
+    )
+      return;
+    if (processData && processData[curProcessDataIdx]) {
+      const curDataKeys = Object.keys(processData[curProcessDataIdx]);
+      const newDates = curDataKeys.map((res) => {
+        const curdates = new Date(res);
+        return `${curdates.getDate()}`;
+      });
+      newDates.unshift('');
+      setDates(newDates);
+    }
+  };
+  function getAvailableSeminarRooms(
+    data: RoomData,
+    date: string,
+    startTime: number,
+    endTime: number,
+    numberOfPeople: number,
+    isOpenSeminaRoom: boolean,
+  ): string[] {
+    const availableRooms = [];
+    console.log('data', data);
+    for (const [room, timeRanges] of Object.entries(data[date])) {
+      console.log('aaa', room, timeRanges);
+      // 세미나실이 예약되어 있지 않은 경우
+      if (isOpenSeminaRoom && parseInt(room) < 10) {
+        continue;
+      }
+      if (!isOpenSeminaRoom && parseInt(room) > 10) {
+        continue;
+      }
+
+      // 해당 세미나실의 사용 가능 인원 확인
+      const availablePeople = SEMINA_AVAILABLE_PEOPLE[parseInt(room)];
+      if (!availablePeople || !availablePeople.includes(numberOfPeople)) {
+        continue;
+      }
+
+      if (timeRanges === null) {
+        availableRooms.push(room + '번');
+      } else {
+        // 예약된 시간대와 겹치지 않는지 확인
+        let isAvailable = true;
+        for (const [reservedStartTime, reservedEndTime] of timeRanges) {
+          console.log(timeRanges);
+          console.log(startTime, reservedStartTime, endTime, reservedEndTime);
+          if (
+            (startTime >= reservedStartTime && startTime < reservedEndTime) ||
+            (endTime > reservedStartTime && endTime <= reservedEndTime) ||
+            (startTime <= reservedStartTime && endTime >= reservedEndTime)
+          ) {
+            isAvailable = false;
+            break;
+          }
+        }
+        if (isAvailable) {
+          availableRooms.push(room + '번');
+        }
+      }
+    }
+    return availableRooms;
+  }
+
+  useEffect(() => {
+    getDays();
+  }, [processData, curProcessDataIdx]);
+
+  useEffect(() => {
+    if (!curCompanions) return;
+    setPersonCount(curCompanions.length);
+  }, [curCompanions]);
+
+  useEffect(() => {
+    getScheduleData([]);
+  }, [selectedSlots]);
+
+  const handleReserveError = () => {
+    setIsError({ isError: false, errorMessage: '' });
+    setIsSelected(false);
+  };
+
+  const handleReserveSuccess = () => {
+    setIsSuccess(false);
+  };
+
+  useEffect(() => {
+    if (roomData === undefined) return;
+    if (selectedSlots.length === 0) return;
+    console.log(
+      selectedSlots[selectedSlots.length - 1].split('-')[3].slice(0, 2),
+    );
+    const availableRooms = getAvailableSeminarRooms(
+      roomData,
+      selectedSlots[0].slice(0, 10),
+      parseInt(selectedSlots[0].split('-')[3].slice(0, 2)),
+      parseInt(
+        selectedSlots[selectedSlots.length - 1].split('-')[3].slice(0, 2),
+      ),
+      personCount + 1,
+      false,
+    );
+    setSeminaRoom(availableRooms);
+    console.log(availableRooms);
+  }, [roomData]);
+
+  useEffect(() => {
+    if (companions) {
+      const decodedCompanions = decodeURIComponent(companions as string);
+      const parseCompanions = JSON.parse(decodedCompanions);
+      setCurCompanions(parseCompanions);
+    }
+  }, []);
+
+  const clickRightArrowBtn = () => {
+    if (processData.length - 1 === curProcessDataIdx) return;
+    setCurProcessDataIdx((res) => res + 1);
+  };
+  const clickLeftArrowBtn = () => {
+    if (processData.length === 0) return;
+    setCurProcessDataIdx((res) => res - 1);
+  };
+
+  return (
+    <Container>
+      <NavHeader title="예약하기" />
+      <HeaderDiv>
+        <HeaderEachDiv>
+          <HeaderDivBoldText>사용시간</HeaderDivBoldText>
+          <HeaderDivText>120분</HeaderDivText>
+        </HeaderEachDiv>
+        <HeaderEachDiv>
+          <HeaderDivBoldText>인원</HeaderDivBoldText>
+          <HeaderDivText>6명</HeaderDivText>
+        </HeaderEachDiv>
+        <HeaderEachDiv>
+          <HeaderDivBoldText>장소종류</HeaderDivBoldText>
+          <HeaderDivText>세미나실</HeaderDivText>
+        </HeaderEachDiv>
+      </HeaderDiv>
+      <Main>
+        <CenterBox>
+          <LeftBox>
+            {curProcessDataIdx === 0 ? (
+              <div style={{ width: '30px' }}></div>
+            ) : (
+              <ArrowBox>
+                <LeftArrow onClick={clickLeftArrowBtn} />
+              </ArrowBox>
+            )}
+          </LeftBox>
+          <TableContainBox>
+            <Schedule
+              processData={processData}
+              isOpenSeminar={isOpenSeminar}
+              curProcessDataIdx={curProcessDataIdx}
+              isSelected={isSelected}
+              setIsSelected={setIsSelected}
+              selectedSlots={selectedSlots}
+              setSelectedSlots={setSelectedSlots}
+              curTime={time as string}
+              dates={dates}
+            />
+          </TableContainBox>
+          <RightBox>
+            {processData.length === 0 ||
+            curProcessDataIdx === processData.length - 1 ? (
+              <div style={{ width: '30px' }}></div>
+            ) : (
+              <ArrowBox>
+                <RightArrow onClick={clickRightArrowBtn} />
+              </ArrowBox>
+            )}
+          </RightBox>
+        </CenterBox>
+      </Main>
+      {isSelected && !isSuccess && !isError.isError && (
+        <ReserveConfirmBottomModal
+          type={ROOM_USE_SECTION[useCaseQuery]}
+          setIsSuccess={setIsSuccess}
+          setIsError={setIsError}
+          semina={seminaRoom}
+          setIsOpen={setIsSelected}
+          selectedSlots={selectedSlots}
+          companions={curCompanions}
+        />
+      )}
+      {isSuccess && (
+        <ConfirmModal
+          onClick={handleReserveSuccess}
+          title="예약이 완료되었습니다."
+          message="예약 정보는 스케줄 탭에서 확인하세요!"
+        />
+      )}
+      {isError.isError && (
+        <ConfirmModal
+          onClick={handleReserveError}
+          title="예약에 실패하였습니다."
+          message={isError.errorMessage}
+        />
+      )}
+    </Container>
+  );
+};
+
+export default Timetable;
+
+const Container = styled.div`
+  position: relative;
+  margin: 6rem auto;
+`;
+
+const Main = styled.main`
+  width: 100%;
+  padding: 1rem 0 0 0;
+  height: 100%;
+`;
+
+const CenterBox = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2.7rem;
+`;
+
+const LeftBox = styled.div`
+  display: flex;
+  margin: auto 0.5rem auto 0;
+`;
+
+const RightBox = styled.div`
+  display: flex;
+  margin: auto 0 auto 0.5rem;
+`;
+
+const TableContainBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 36rem;
+`;
+
+// const FilterChild = styled.div`
+//   margin-top: 24px;
+//   margin-bottom: 20px;
+// `;
+const ArrowBox = styled.div`
+  width: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderDiv = styled.div`
+  width: 100%;
+  padding: 15px 36px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ececec;
+  background: #feffff;
+`;
+
+const HeaderEachDiv = styled.div`
+  display: flex;
+  /* justify-content: ; */
+  align-items: center;
+`;
+
+const HeaderDivBoldText = styled.div`
+  ${TYPO.text1.Lg}
+  ${COLORS.grey0}
+`;
+
+const HeaderDivText = styled.div`
+  ${TYPO.text1.Lg}
+  color:#ccc;
+  margin-left: 10px;
+`;

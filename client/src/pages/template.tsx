3121a0b823ac69a69fa3a98131e877d6f24ab68c
Jun99uu_Lee <igun0423@naver.com>
2023-11-29 14:05:10 +0900

    fix: template main page interaction

--- client/src/pages/template.tsx
+++ client/src/pages/template.tsx
@@ -1,7 +1,18 @@
 import AddTemplate from '@/components/AddTemplate';
+import { PageContainer } from '@/styles/tokens';
+import { css } from '@emotion/react';
 
 const Template = () => {
-  return <AddTemplate />;
+  return (
+    <PageContainer css={pageStyle}>
+      <AddTemplate />
+    </PageContainer>
+  );
 };
+
+const pageStyle = css`
+  width: 100%;
+  padding: 3rem 0rem;
+`;
 
 export default Template;

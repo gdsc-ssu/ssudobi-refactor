3ed0aaacf512497a4fe22516726db7d74179777b
Jun99uu_Lee <igun0423@naver.com>
2023-12-28 00:37:55 +0900

    remove: type error part

--- client/src/components/TemplateList/HomeTemplate/HomeTemplate.stories.tsx
+++ client/src/components/TemplateList/HomeTemplate/HomeTemplate.stories.tsx
@@ -10,14 +10,3 @@ const meta = {
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-
-export const Reserve: Story = {
-  args: {
-    title: '템플릿 제목',
-    beginTime: '목요일 13시',
-    endTime: '15시 30분',
-    place: '세미나룸 102호',
-    memo: '슈도비 회의 잊지말기',
-    friends: ['정명진 / 20181234', '이준규 / 20181234'],
-  },
-};
